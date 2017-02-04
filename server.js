632eb53f8879e8176d3f62d523e93df58738d87d
Hadi Yousufi <reach.hadiy@gmail.com>
2017-02-04 14:09:53 -0600

    2 schema models/tables created, Users and Posts. functionally workin

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ var db = require("./models");
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({type: "application/vnd.api+json"}));
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Sets up Express to use Passport for authentication
 app.use(passport.initialize());
@@ -27,8 +27,8 @@ require("./routes/html-routes.js")(app);
 // require("./routes/api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our express app
-db.sequelize.sync({force: true}).then(function () {
-    app.listen(PORT, function () {
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
         console.log("App listening on PORT " + PORT);
     });
 });
