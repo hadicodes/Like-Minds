4e4e86db5202ed64f37a57e43883cacbc47b1529
Cassie <cassie.pericak@northwestern.edu>
2017-02-04 14:27:56 -0600

    built html pages for basic layout

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,17 +2,27 @@ var path = require("path");
 var passport = require('passport');
 var Strategy = require('passport-local').Strategy;
 
-module.exports = function (app) {
-    app.get("/login", function (req, res) {
+module.exports = function(app) {
+    app.get("/login", function(req, res) {
         res.sendFile(path.join(__dirname + "/../public/login.html"));
     });
 
+    // app.get("/topics", function(req, res) {
+  //     res.render("index", { index: dbBurgers });
+  // });
+
     passport.use(new Strategy(function(username, password, cb) {
-    	db.users.findByUsername(username, function(err, user) {
-    		if (err) { return cb(err); }
-	    	if (!user) { return cb(null, false); }
-    		if (user.password != password) { return cb(null, false); }
-	    	return cb(null, user);
-    	});
-  	}));
+        db.users.findByUsername(username, function(err, user) {
+            if (err) {
+                return cb(err);
+            }
+            if (!user) {
+                return cb(null, false);
+            }
+            if (user.password != password) {
+                return cb(null, false);
+            }
+            return cb(null, user);
+        });
+    }));
 };
