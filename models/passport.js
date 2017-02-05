ccd3d6ff2aa860b0ea5e8e5873ecdb1ef09298a0
kjige <chrisleongsc2@gmail.com>
2017-02-05 11:59:01 -0600

    register user and login

---
+++ models/passport.js
@@ -0,0 +1,22 @@
+var Sequelize = require('sequelize');
+var passportLocalSequelize = require('passport-local-sequelize');
+
+module.exports = function (sequelize, DataTypes) {
+    var User = sequelize.define('User', {
+        username: DataTypes.STRING,
+        hash: {
+            type: DataTypes.TEXT
+        },
+        salt: {
+            type: DataTypes.STRING
+        }
+    });
+
+    passportLocalSequelize.attachToUser(User, {
+        usernameField: 'username',
+        hashField: 'hash',
+        saltField: 'salt'
+    });
+
+    return User;
+};
