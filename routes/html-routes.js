99907f1e5b4d127911bbc06b344d190648c63e77
kjige <chrisleongsc2@gmail.com>
2017-02-05 12:36:15 -0600

    server force false

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -11,7 +11,7 @@ module.exports = function (app) {
 
     app.post('/login', 
         passport.authenticate('local', {
-            successRedirect: '/',
+            successRedirect: '/forum',
             failureRedirect: '/login'
             // If this function gets called, authentication was successful.
             // `req.user` contains the authenticated user.
