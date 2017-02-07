aca10865a89eeba204b3f8b9f2fe7b43f4c16623
Subhan Ali <ali.023@gmail.com>
2017-02-06 21:43:05 -0600

    created changing modal from login --> register --> lost pw(3)

--- public/js/login.js
+++ public/js/login.js
@@ -2,21 +2,45 @@ console.log('login file opened');
 
 var login = $('.username').text;
 var pw = $('.password').text;
+var $modalAnimateTime = 300;
+$(document).ready(function() {
+    $('.sign-in').click(function() {
+        console.log('sign-in button pressed');
+        $('#sign-in-modal').modal("show");
+        $('#lost-form').modal('hide');
+        $('#register-form').modal('hide');
+        $('#login-form').modal('show');
+        $.ajax({
+                method: 'POST',
+                url: '/login',
+                data: {
+                    username: login,
+                    password: pw
+                }
+            })
+            .done(function() {
+                console.log('returned');
+            });
+    });
+    $('.login_login_btn').click(function() {
+        $('#lost-form').modal('hide');
 
-$('#loading-image').css('display', '""');
+        $('#register-form').modal('hide');
 
-$('.sign-in').click(function() {
-    console.log('sign-in button pressed');
-    $.ajax({
-            method: "POST",
-            url: "/login",
-            data: {
-                username: login,
-                password: pw
-            }
-        })
-        .done(function() {
-            $('#loading-image').css('display', 'none');
-        });
-    $('#sign-in-modal').show();
+        $('#login-form').modal('show');
+    });
+    $('.login_lost_btn').click(function() {
+        $('#login-form').modal('hide');
+
+        $('#register-form').modal('hide');
+
+        $('#lost-form').modal('show');
+    });
+    $('.login_register_btn').click(function() {
+        $('#lost-form').modal('hide');
+
+        $('#login-form').modal('hide');
+
+        $('#register-form').modal('show');
+    });
 });
