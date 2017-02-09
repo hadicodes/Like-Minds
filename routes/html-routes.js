1224f2c22260fdab48073831ab976f6832019f3f
Subhan Ali <ali.023@gmail.com>
2017-02-08 21:19:44 -0600

    created/updatedAt updates

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,8 +5,8 @@ var db = require('../models');
 var account = db.account;
 
 
-module.exports = function (app) {
-    app.get("/login", function (req, res) {
+module.exports = function(app) {
+    app.get("/login", function(req, res) {
 
 
         res.sendFile(path.join(__dirname + "/../public/login.html"));
@@ -16,13 +16,12 @@ module.exports = function (app) {
         passport.authenticate('local', {
             // failureRedirect: '/login'
         }),
-        function (req, res) {
+        function(req, res) {
             // If this function gets called, authentication was successful.
             // `req.user` contains the authenticated user.
             res.json(req.user);
         });
 
-<<<<<<< HEAD
     // app.post('/register', function(req, res) {
     //     User.register(req.body.username, req.body.password, function(err, account) {
     //         if (err) {
@@ -32,32 +31,21 @@ module.exports = function (app) {
     //         res.json(account);
     //     });
     // });
-=======
-    app.post('/register', function (req, res) {
-        account.register(req.body.username, req.body.password, function (err, account) {
-            if (err) {
-                console.log(err);
-                return res.json(err);
-            }
-            res.json(account);
-        });
-    });
->>>>>>> master
 
 
-    app.get("/", function (req, res) {
+    app.get("/", function(req, res) {
         res.sendFile(path.join(__dirname + "/../public/login.html"));
     });
 
-    app.get("/forum", function (req, res) {
+    app.get("/forum", function(req, res) {
         res.sendFile(path.join(__dirname + "/../public/forum.html"));
     });
 
-    app.get("/newpost", function (req, res) {
+    app.get("/newpost", function(req, res) {
         res.sendFile(path.join(__dirname + "/../public/newpost.html"));
     });
 
-    app.get("/topics", function (req, res) {
+    app.get("/topics", function(req, res) {
         res.sendFile(path.join(__dirname + "/../public/topics.html"));
     });
 };
