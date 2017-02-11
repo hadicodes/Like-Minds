90c341a9b84dafeeda3ae233507f93c562411afd
kjige <chrisleongsc2@gmail.com>
2017-02-10 20:47:16 -0600

    fixed redirect after login

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,15 +3,16 @@ var passport = require('passport');
 var Strategy = require('passport-local').Strategy;
 var db = require('../models');
 var account = db.account;
+var User = db.User;
 
 
-module.exports = function(app) {
-    app.get("/login", function(req, res) {
+module.exports = function (app) {
+    app.get("/login", function (req, res) {
         res.sendFile(path.join(__dirname + "/../public/login.html"));
     });
 
-    passport.use(new Strategy(function(username, password, cb) {
-        db.account.findByUsername(username, function(err, user) {
+    passport.use(new Strategy(function (username, password, cb) {
+        db.account.findByUsername(username, function (err, user) {
             if (err) {
                 return cb(err);
             }
@@ -30,14 +31,21 @@ module.exports = function(app) {
                 successRedirect: '/forum',
                 failureRedirect: '/login'
             },
-            function(req, res) {
+            function (req, res) {
                 console.log('RESPONSE ' + res);
             }
         )
     );
 
-    app.post('/register', function(req, res) {
-        User.register(req.body.username, req.body.password, function(err, account) {
+    app.post('/register', function (req, res) {
+        console.log("USER " + req.body.username, req.body.email, req.body.location, req.body.interests);
+        User.create({
+            username: req.body.username,
+            email: req.body.email,
+            location: req.body.location,
+            interests: req.body.interests
+        });
+        account.register(req.body.username, req.body.password, function (err, account) {
             if (err) {
                 console.log(err);
                 return res.json(err);
@@ -46,31 +54,37 @@ module.exports = function(app) {
         });
     });
 
-    app.get("/forum", function(req, res) {
+    app.get("/forum", function (req, res) {
         db.Post.findAll({
             attributes: ["topic"]
-        }).then(function(dbForumTopics) {
-            res.render("forum", { topic: dbForumTopics });
+        }).then(function (dbForumTopics) {
+            res.render("forum", {
+                topic: dbForumTopics
+            });
         });
     });
 
-    app.get("/threads", function(req, res) {
+    app.get("/threads", function (req, res) {
         db.Post.findAll({
             attributes: ["thread_title"]
-        }).then(function(dbThreadTitle) {
-            res.render("threads", { thread_title: dbThreadTitle });
+        }).then(function (dbThreadTitle) {
+            res.render("threads", {
+                thread_title: dbThreadTitle
+            });
         });
     });
 
-    app.get("/posts/", function(req, res) {
+    app.get("/posts/", function (req, res) {
         db.Post.findAll({
             attributes: ["thread_message"]
-        }).then(function(dbPosts) {
-            res.render("posts", { thread_message: dbPosts });
+        }).then(function (dbPosts) {
+            res.render("posts", {
+                thread_message: dbPosts
+            });
         });
     });
 
-    app.get("/newthread", function(req, res) {
+    app.get("/newthread", function (req, res) {
         res.render("newthread");
     });
 };
