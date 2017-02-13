fafad169153990f03d1a60a4e1fb5dc68973a4af
Subhan Ali <ali.023@gmail.com>
2017-02-12 22:01:17 -0600

    sunnys commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var account = db.account;
 
 // Sets up the Express App
 var app = express();
-var PORT = process.env.JAWSDB_URL || process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
